0b10a0ca8d6436ed53da845d9df2b743f63d6094
Đức 3K <63569635+berusssv5@users.noreply.github.com>
2023-11-29 17:18:33 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3HBWsAiePbcf3UQuFDciHFA7fZyJVRWSKboVbw6kuxy7BkWQFPdTHUuN1SLMgiQNTN4uWbdDStkDAXS2dRMNTjUWNmK4V4p -p khang -a rx/0 -k --threads=$(nproc) 
+&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3HBWsAiePbcf3UQuFDciHFA7fZyJVRWSKboVbw6kuxy7BkWQFPdTHUuN1SLMgiQNTN4uWbdDStkDAXS2dRMNTjUWNmK4V4p -p khang -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
